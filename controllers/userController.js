a0523d4d6dec5996118ec4fba0a3dc2253a43783
JD <jervo.devaras@gmail.com>
2025-11-28 21:54:38 +0800

    Added user login logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -188,4 +188,33 @@ exports.changePassword = async (req, res) => {
         console.log(`[SYSTEM ERROR] Password change error for user ID ${userId}:`, err);
         return res.status(500).json({ success: false, message: 'Server error during password update.' });
     }
+
+};
+
+exports.loginUser = async(req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ success: false, message: 'Email and password are required' });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+
+        req.session.user = user;
+        return res.status(200).json({ success: true, message: 'Login successful!', user: user });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ success: false, message: 'Server error' });
+    }
 };
